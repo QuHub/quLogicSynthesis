65d03790f349ff8a6b8ed94a4fc6692191c7e7a3
Maher Hawash <gmhawash@gmail.com>
2012-07-05 05:41:11 -0700

    switch from rando server to Windows psudo-random generator

--- quLogicSynthesis/Utilities/Rand.cpp
+++ quLogicSynthesis/Utilities/Rand.cpp
@@ -1,74 +1,10 @@
-#include "StdAfx.h"
-#include <windows.h>
-#include "Rand.h"
-
-
-using namespace std;
-using namespace System;
-using namespace System::Collections::Generic;
-using namespace System::Text::RegularExpressions;
-using namespace System::Text;
-using namespace System::Net;
-using namespace System::IO;
-#using <mscorlib.dll>
-
-using namespace System;
-using namespace System::Collections;
-
-#define BufferSize  (sizeof(m_numbers) / sizeof(m_numbers[0]))
-
+#include "stdafx.h"
+#pragma once
+#include <vcclr.h>
+using namespace System::Reflection;
 namespace Rand
 {
-  Rand *m_pRandom;
-
-  Rand::Rand(void)
-  {
-    Fill();
-    m_reader = 0;
-  }
-
-  DWORD Rand::Run(LPVOID args) 
-  { 
-    while(true) {
-      Fill();
-      Sleep(10);
-    }
-
-    Console::WriteLine("Why am I exiting");
-    return true;
-  }
-
-  using namespace System::Runtime::InteropServices;
-  void Rand::Fill()
-  {
-    Console::WriteLine("Fetching True Random Numbers from RandomServer \n");
-    WebRequest^ req = WebRequest::Create("http://www.randomserver.dyndns.org/client/random.php?type=LIN&a=0&b=1&file=0&n=" + BufferSize);
-    HttpWebResponse^ resp = dynamic_cast<HttpWebResponse^>(req->GetResponse());
-
-    StreamReader^ reader = gcnew StreamReader(resp->GetResponseStream());
-    // Skip first two lines
-    String^ line = reader->ReadLine();
-    line = reader->ReadLine();
-
-    line = reader->ReadLine();
-
-    for(int i=0; i<BufferSize/2; i++) {
-      Extract(line, 2*i);
-      line = reader->ReadLine();
-    }
-  }
-
-  double Rand::Double()
-  { 
-    m_reader = ++m_reader % BufferSize; // circular buffer
-    return m_numbers[m_reader];
-  }
-#pragma managed  // This stupid thing is necessary to be able to use regex matches.
-  void Rand::Extract(String^ line, int index)
-  {
-    MatchCollection^ m = Regex::Matches(line, "(\\d.\\d*)");
-    m_numbers[index++] = Convert::ToDouble(m[0]->Result("$1"));
-    m_numbers[index++] = Convert::ToDouble(m[1]->Result("$1"));
-  }
-
-}
+  gcroot<Random^> m_rnd = gcnew Random();
+  double Double() {return m_rnd->NextDouble();}
+  int Integer(int n) {return m_rnd->Next(n);}
+};
--- quLogicSynthesis/Utilities/Rand.h
+++ quLogicSynthesis/Utilities/Rand.h
@@ -1,26 +1,9 @@
 #pragma once
-#include "windows.h"
-#include "Thread.h"
-
+#include <vcclr.h>
+using namespace System::Reflection;
 namespace Rand
 {
-  public class Rand : public CThread
-  {
-  protected:
-    double m_numbers[10000];
-    int m_reader;
-  public:
-    Rand(void);
-
-    void Fill();
-    virtual DWORD Run(LPVOID args);
-    void Rand::Extract(String^ line, int index);
-    double Double(); 
-  };
-
-  extern Rand *m_pRandom;
-
-  inline void Initialize() { m_pRandom = new Rand(); m_pRandom->Start(NULL);}
-  inline double Double() {return m_pRandom->Double();}
-  inline int Integer(int range=INT_MAX) {return (int)(range*m_pRandom->Double());}
-}
+  void Initialize() {}
+  double Double();
+  int Integer(int n);
+};
